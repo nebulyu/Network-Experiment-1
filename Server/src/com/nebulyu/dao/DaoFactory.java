a11a82998603326b8a0a68209e8ba218937ab4a1
nebulyu <1192398570@qq.com>
2024-05-07 15:15:50 +0800

    update

--- Server/src/com/nebulyu/dao/DaoFactory.java
+++ Server/src/com/nebulyu/dao/DaoFactory.java
@@ -7,20 +7,11 @@ import java.util.Map;
 
 public class DaoFactory {
     private static Map<String, UserDao> userDaoMap = new HashMap<String, UserDao>();
-    public static void initDao()
-    {
+    public static void initDao(){
         userDaoMap.put("sqlite",new UserDaoSqlite());
     }
     //	private static String DBTYPE = "oracle";
     public static UserDao getUserDao(){
-        if (Config.DBTYPE.equals("sqlite")) {
-            return new UserDaoSqlite();
-        } else if (Config.DBTYPE.equals("oracle")) {
-           // return new UserDaoOracle();
-        } else if (Config.DBTYPE.equals("mysql")) {
-            //return new UserDaoMySql();
-        }
-        return null;
-
+        return new UserDaoSqlite();
     }
 }
